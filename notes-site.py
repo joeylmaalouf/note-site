56530ebfa39da52c8b1353ded1624e3ce2240b3e
joeylmaalouf <joeylmaalouf@gmail.com>
2015-05-27 23:54:54 -0400

    basic features

--- notes-site.py
+++ notes-site.py
@@ -1,11 +1,29 @@
+from os.path import exists
 import random
 import string
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 
 def generate_random_id(length):
 	return "".join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits) for i in range(length))
+
+
+def save_note_contents(note_id, contents):
+	f = open("notepads/{0}.txt".format(note_id), "w")
+	f.write(contents)
+	f.close()
+
+
+def load_note_contents(note_id):
+	path = "notepads/{0}.txt".format(note_id)
+	if exists(path):
+		f = open(path, "r")
+		s = f.read()
+		f.close()
+		return s
+	else:
+		return "Welcome to your notepad!\n\nWhatever you type here will be persistent for this notepad,\nspecified by the ID in the URL. This pad's ID is {0}.\n".format(note_id)
 
 
 @app.route("/")
@@ -15,10 +33,10 @@ def index():
 
 @app.route("/<note_id>/")
 def show_note(note_id):
-	return "<textarea style=\"width:97.5vw; height:97.5vh; margin:auto; display:block; resize:none\">Welcome to your notepad!\nThis pad's ID is "+str(note_id)+".</textarea>"
-	# TODO: figure out nicer styling, inline is ugly
-	# TODO: automatically save and load notepad contents in ./notepads/<note_id>
+	return render_template("pad.html", note_id = note_id, note_text = load_note_contents(note_id))
+	# TODO: automatically save notepad contents in ./notepads/<note_id>
+	# TODO: remove debug mode
 
 
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0")
+	app.run(host = "0.0.0.0", debug = True)
