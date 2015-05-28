4a55a9d0b4d9c6a7ec463eec45dbe947514e0e07
joeylmaalouf <joeylmaalouf@gmail.com>
2015-05-28 19:50:21 -0400

    trying to get save working again

--- notes-site.py
+++ notes-site.py
@@ -1,9 +1,9 @@
-
 import os
 import random
 import string
 from flask import Flask, redirect, render_template, request
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+app = Flask(__name__, template_folder=tmpl_dir)
 
 
 def generate_random_id(length):
